2fb4a38309a09dc7e73b617f572c709653e17619
AAMTek <64968842+AAMTek@users.noreply.github.com>
2020-08-05 17:29:51 +0000

    Add files via upload

---
+++ module6 making decisions with code.py
@@ -0,0 +1,7 @@
+EidHoliday = input("Is Monday the Eidul fitr holiday? ")
+##if EidHoliday == "yes" :
+#    print("Huraaaayyyyy")
+#    print("Plan your activities for that holiday")
+##if EidHoliday != "yes" :
+##    Print("Sorry)
+##    Print("Go to work")
